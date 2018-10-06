db3b283b97f685e98e8271a8706fb3dc78ded989
Rushy Patel <rushy.bba@gmail.com>
2018-10-06 16:46:45 +0530

    init

--- src/com/javatpoint/Test.java
+++ src/com/javatpoint/Test.java
@@ -28,5 +28,8 @@ public static void main(String[] args) {
     	set.remove(i-1);
     }
     System.out.println(">>>>>>>>>>>>>>>>>"+set.size());
-}
+    
+    //Version 2
+    student.displayInfo();
+	}
 }
