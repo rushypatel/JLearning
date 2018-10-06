b18c2a96cf59d2be0dc9dd1f5decf82d4f597183
Rushit Vaishnani <>
2018-10-06 18:53:01 +0530

    added in tutor

--- src/com/javatpoint/Tutor.java
+++ src/com/javatpoint/Tutor.java
@@ -2,6 +2,21 @@ package com.javatpoint;
 
 public class Tutor {
 	private String name;
+	private String subject;
+
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
 
 	public String getName() {
 		return name;
