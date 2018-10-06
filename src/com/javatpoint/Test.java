eb573f19f46008d31a32720a8961732a9063e2c7
Rushy Patel <rushy.bba@gmail.com>
2018-10-06 15:50:27 +0530

    init

---
+++ src/com/javatpoint/Test.java
@@ -0,0 +1,32 @@
+package com.javatpoint;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+public static void main(String[] args) {
+	Resource resource=new ClassPathResource("applicationContext.xml");
+	BeanFactory factory=new XmlBeanFactory(resource);
+	
+	Student student=(Student)factory.getBean("studentbean");
+	student.displayInfo();
+	Short sObj2 = new Short("10");
+	   
+    //print value of Short objects
+    //System.out.println(sObj1);
+    System.out.println(sObj2);
+    
+    Set<Short> set = new HashSet<Short>();
+    for(short i=0;i<10;i++){
+    	System.out.println(">>>"+i);
+    	set.add(i);
+    	set.remove(i-1);
+    }
+    System.out.println(">>>>>>>>>>>>>>>>>"+set.size());
+}
+}
