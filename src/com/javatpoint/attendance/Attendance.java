16bf273d55de2537c450a6c2a3e68f6ea335df5d
Rushy Patel <rushy.bba@gmail.com>
2019-04-05 00:12:24 +0530

    merged class

--- src/com/javatpoint/attendance/Attendance.java
+++ src/com/javatpoint/attendance/Attendance.java
@@ -3,7 +3,8 @@ package com.javatpoint.attendance;
 public class Attendance {
 	private int id;
 	private String type;
-	
+	private String name;
+
 	public int getId() {
 		return id;
 	}
@@ -15,6 +16,14 @@ public class Attendance {
 	}
 	public void setType(String type) {
 		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }
