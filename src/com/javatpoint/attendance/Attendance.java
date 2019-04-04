e7a93a36d2b156191176ceaef573bb017a058fa1
Rushy Patel <rushy.bba@gmail.com>
2019-04-05 00:06:18 +0530

    added id and type

--- src/com/javatpoint/attendance/Attendance.java
+++ src/com/javatpoint/attendance/Attendance.java
@@ -1,5 +1,20 @@
 package com.javatpoint.attendance;
 
 public class Attendance {
-
+	private int id;
+	private String type;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }
